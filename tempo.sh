f399ba4338f025f65492146ee43e365edc9eaf30
Randolph Roble <r.roble@arcanys.com>
2020-02-19 15:17:06 +0800

    Update ignore

--- tempo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -i \
-    -X POST \
-    -H "Authorization: Bearer ${TOKEN}" \
-    -H "Content-Type: application/json" \
-    -d @worklog.json \
-    https://api.tempo.io/core/3/worklogs
